f02d48a3784872563b305d04c332095cd3043dd6
Ozguc Bertug Capunaman <ozgucbertug@gmail.com>
2019-03-18 15:56:50 -0400

    Scanner GUI Automated

--- py/app.py
+++ py/app.py
@@ -1,18 +1,22 @@
 from pynput.keyboard import Key, Listener
 import threading
 import time
+import datetime
+import os
 
 from udpComm import UDP
+from scanner import Scanner
 import ply2xyz
 
 class IDF(object):
 
-	def __init__(self, sessionName):
+	def __init__(self):
 		self._done = False
 		self._stateList = ['handshake', 'initGeo']
 		self._state = self._stateList[0]
 		self._cycle = 0
-		self._session = sessionName
+		self._session = None
+		self._mainDIR = "C:\\Users\\Ozguc Capunaman\\Documents\\GitHub\\InteractiveDigitalFabrication\\sessions\\"
 		
 		# UDP
 		self.udp_out = UDP(mode = 'out')
@@ -23,13 +27,20 @@ class IDF(object):
 		self.nozzle_width = 1
 		self.resolution = 50
 
+		self.create_session()
+		self.scanner = Scanner(self._session)
+
 	def on_press(self, key):
 		if key == Key.esc:
 			self._done = True
 			return False
 
-# Collect events until released
-
+	def create_session(self):
+		now = datetime.datetime.now()
+		self._session = now.strftime("%Y%m%d-%H%M")
+		folder = "../sessions/" + self._session
+		if not os.path.isdir(folder):
+			os.mkdir(folder)
 
 	def UDP_send(self, msg):
 		self.udp_out.send(msg)
@@ -39,7 +50,7 @@ class IDF(object):
 
 	def gh_py_handhake(self):
 		print("Handshaking...", end='')
-		self.UDP_send('+gh_handshake')
+		self.UDP_send('+gh_handshake' + "_" + self._session)
 		ret = self.UDP_receive(5)
 		if ret == '-gh_success':
 			self._state = 'initGeo'
@@ -49,7 +60,7 @@ class IDF(object):
 			self._done = True
 
 
-	def toolPathRequest(self, mode):
+	def tool_path_request(self, mode):
 		if mode == "init":
 			print("Requesting Initial Tool Path...", end='')
 			filename = self._session + "-tp" + str(self._cycle)
@@ -67,7 +78,18 @@ class IDF(object):
 				print(" Failed!")
 				self._done = True
 		else:
-			pass
+			#############################################
+			print("Requesting Tool Path for Cycle %d..." %self._cycle, end='')
+			filename = self._session + "-tp" + str(self._cycle)
+			msg = '+gh_initGeo' + "_" + str(self.resolution) + "_" + str(self.layer_height) + "_" + str(filename)
+			self.UDP_send(msg)
+			ret = self.UDP_receive(20)
+			if ret == '-gh_success':
+				self._state = 'readyToPrint'
+				print("Successful!")
+			else:
+				print(" Failed!")
+				self._done = True
 
 	def runHorus(self):
 		pass
@@ -77,15 +99,19 @@ class IDF(object):
 		while not self._done:
 			###################################
 			print("Running IDF")
-			if self._state == 'handshake':
-				self.gh_py_handhake()
-				time.sleep(.1)
-			if self._state == 'initGeo':
-				self.toolPathRequest("init")
-			if self._state == 'readyToPrint':
-				break
+			self.scanner.scan()
+			self.scanner.saveScan("scan" + str(self._cycle))
+			# if self._state == 'handshake':
+			# 	self.gh_py_handhake()
+			# 	time.sleep(.1)
+			# if self._state == 'initGeo':
+			# 	self.tool_path_request("init")
+			# if self._state == 'readyToPrint':
+			# 	break
 			###################################
 			# listener.join()
 
-a = IDF('test')
-a.run()
+a = IDF()
+# a.run()
+
+a.scanner.run("scan" + str(0))
